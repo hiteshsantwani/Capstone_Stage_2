fcb1f2d6907776a5c60af8ae06246961487fe022
hiteshsantwani <hiteshsantwani35@gmail.com>
2017-01-24 02:05:50 +0530

    final project

---
+++ app/src/main/java/com/mindsparkk/ExpertTravel/Utils/MyBoldTextView.java
@@ -0,0 +1,32 @@
+package com.mindsparkk.ExpertTravel.Utils;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by Hitesh on 05/09/15.
+ */
+public class MyBoldTextView extends TextView {
+
+    public MyBoldTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setType(context);
+    }
+
+    public MyBoldTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setType(context);
+    }
+
+    public MyBoldTextView(Context context) {
+        super(context);
+        setType(context);
+    }
+
+    private void setType(Context context) {
+        this.setTypeface(Typeface.createFromAsset(context.getAssets(),
+                "Rosario-Bold.ttf"));
+    }
+}
